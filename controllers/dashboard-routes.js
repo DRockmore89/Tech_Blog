4d10b645868ba6517fdd6e7b916fb37be38b68dd
DRockmore89 <donaldrockmore@yahoo.com>
2021-07-14 14:02:46 -0400

    commit

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const { Post } = require('../models/');
+const withAuth = require('../utils/auth');
+
+//activity 28 controllers/homeRoutes.js.
+//activity 28 controllers/projectRouts.js
+//use withAuth middleware to prevent access to route.
+//if you are logged in, then you can create new blog posts.
+//this way the / route and all its child routes will be protected 
+//by our authentication middleware.
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        userId: req.session.userId,
+      },
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('allPostsAdmin', {
+      layout: 'dashboard',
+      posts,
+    });
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+router.get('/new', withAuth, (req, res) => {
+  res.render('newPost', {
+    layout: 'dashboard',
+  });
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await post.findByPk(req.params.id);
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render('edit-post', {
+        layout: 'dashboard',
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+module.exports = router;
