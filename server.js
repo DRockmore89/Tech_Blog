eae606998c6c4109b498a5537ee2949910548eb3
DRockmore89 <donaldrockmore@yahoo.com>
2021-07-15 06:29:04 -0400

    commit

--- server.js
+++ server.js
@@ -2,29 +2,45 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+const expressHBs = require('express-handlebars');
 const helpers = require('./utils/helpers');
+const Sequelize = require('sequelize');
+
+// const routes = require('./controllers/');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+sequelize.define("Session", {
+  sid: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
+  userId: Sequelize.STRING,
+  expires: Sequelize.DATE,
+  data: Sequelize.TEXT,
+});
 
-const hbs = exphbs.create({ helpers });
-
+function extendDefaultFields(defaults, session) {
+  return {
+    data: defaults.data,
+    expires: defaults.expires,
+    userId: session.userId,
+  };
+}
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'MojoRising',
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
-
 app.use(session(sess));
+const hbs = expressHBs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -32,11 +48,9 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(require('./controllers/'));
 
-app.use(routes);
-
-sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
-  });
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}.`);
+  sequelize.sync({ force: false });
+});
