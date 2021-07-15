f37a230562c96f08fd06ac34d5879102b08f3118
DRockmore89 <donaldrockmore@yahoo.com>
2021-07-15 08:55:56 -0400

    commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const Sequelize = require('sequelize');
 
 // const routes = require('./controllers/');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
