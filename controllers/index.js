530de4a17ac806a9c24a9fd5eb5840e4750fc8c8
DRockmore89 <donaldrockmore@yahoo.com>
2021-07-15 08:40:36 -0400

    commit

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,8 @@ const homeRoutes = require('./homeRoutes.js');
 const dashBdRoutes = require('./dashboardRoutes.js');
 const apiRoutes = require('./api/');
 
-router.use('./', homeRoutes);
-router.use('./dashboard', dashBdRoutes);
-router.use('./api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashBdRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
