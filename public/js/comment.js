a345d02a2c8aa78a7f8aedda891f573efed387ad
DRockmore89 <donaldrockmore@yahoo.com>
2021-07-15 05:58:40 -0400

    commit

---
+++ public/js/comment.js
@@ -0,0 +1,23 @@
+const commentFormHandler = async function (event) {
+    event.preventDefault();
+  
+    const commentBody = document.querySelector(
+      'textarea[name="comment-body"]'
+    ).value;
+    const postID = document.querySelector('input[name="post-id"]').value;
+  
+    if (commentBody) {
+      await fetch('/api/comment', {
+        method: 'POST',
+        body: JSON.stringify({
+          postID,
+          commentBody,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      document.location.reload();
+    }
+  };
+  document
+    .querySelector('#new-comment-form')
+    .addEventListener('submit', commentFormHandler);
